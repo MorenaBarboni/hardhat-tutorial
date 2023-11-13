3b41c57ff8ccc9d2a99339dc5a7af74f865d6f3f
morenabarboni <morena.barboni@unicam.it>
2023-11-13 16:10:07 +0100

    Token.js solution

--- test/Token.js
+++ test/Token.js
@@ -2,6 +2,9 @@ const { expect } = require("chai");
 
 describe("Token contract", function () {
  it("Deployment should assign the total supply of tokens to the owner", async function () {
-   //write your test here
+    const [owner] = await ethers.getSigners();
+    const hardhatToken = await ethers.deployContract("Token");
+    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
  });
 });
