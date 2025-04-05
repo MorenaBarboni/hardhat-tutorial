498bd76be6b10c37ec421ce77bfeeb41c83ccec9
morenabarboni <morena.barboni@unicam.it>
2025-04-05 16:08:20 +0200

    add sumo config

---
+++ CampusCoin/sumo-config.js
@@ -0,0 +1,12 @@
+module.exports = {  
+      buildDir: "auto",
+      contractsDir: "auto",
+      testDir: "auto",
+      skipContracts: ["interfaces", "mock", "test"],
+      skipTests: [],
+      testingFramework: "auto",
+      minimalOperators: false,
+      randomSampling: false,
+      randomMutants: 100,
+      testingTimeOutInSec: 500  
+}
