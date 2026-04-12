31852f23d66b52ea9e9e2a73509ba1617fc5f194
morenabarboni <morena.barboni@unicam.it>
2026-04-12 16:18:31 +0200

    Add hardhat-abi-exporter

--- Token/hardhat.config.js
+++ Token/hardhat.config.js
@@ -1,5 +1,13 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-abi-exporter");
+
 module.exports = {
   solidity: "0.8.19",
+  abiExporter: {
+    path: "./abi",
+    runOnCompile: true,
+    clear: true,
+    language: "json"
+  }
 };
